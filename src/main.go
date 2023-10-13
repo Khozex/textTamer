393293cba3292bed45f16c5a5b9d52a293cceeb3
Khozex <ismaelteles75@gmail.com>
2023-10-13 14:18:47 -0300

    create a flag system to get input by file

--- src/main.go
+++ src/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func breakTextIntoWords(text string) []string {
 	var words []string
 	var word string
-	//split using regex
 	for _, char := range text {
 		if string(char) == " " {
 			words = append(words, word)
@@ -22,6 +24,23 @@ func breakTextIntoWords(text string) []string {
 	return words
 }
 
+func readToFile(path string) string {
+	absPath, _ := filepath.Abs(path)
+	file, err := os.Open(absPath)
+	if err != nil {
+		fmt.Println("Error reading file")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var text []string
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+	return strings.Join(text, " ")
+}
+
 func parseWordToBold(text string) string {
 	wordLength := len(text)
 	halfWordLength := wordLength / 2
@@ -30,6 +49,18 @@ func parseWordToBold(text string) string {
 }
 
 func main() {
+	var filePathFlag = flag.String("f", "", "path to file")
+	flag.Parse()
+	if *filePathFlag != "" {
+		text := readToFile(*filePathFlag)
+		words := breakTextIntoWords(text)
+		var bionicWords []string
+		for _, word := range words {
+			bionicWords = append(bionicWords, parseWordToBold(word))
+		}
+		fmt.Print(bionicWords)
+		return
+	}
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
 	words := breakTextIntoWords(text)
