2a4dbef0f03aa2f5ad598f1a398a92c7052803a8
Khozex <ismaelteles75@gmail.com>
2023-10-24 22:50:21 -0300

    create a flag system to main program

--- src/main.go
+++ src/main.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"os"
 	"textTamer/lib"
 )
 
+func setFlags() {
+	flag.Int("f", 50, "Porcent of word to bold")
+	flag.String("t", "md", "Type of bold")
+	flag.Int("j", 0, "Jumps of word to bold")
+	flag.Parse()
+}
+
 func main() {
+	setFlags()
+	flag.Parse()
 	var stdinWord string
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		stdinWord += scanner.Text()
 	}
 	args := lib.BoldArgs{
-		TypeBold: "md",
-		Porcent:  50,
-		Jumps:    0,
+		TypeBold: flag.Lookup("t").Value.(flag.Getter).Get().(string),
+		Porcent:  flag.Lookup("f").Value.(flag.Getter).Get().(int),
+		Jumps:    flag.Lookup("j").Value.(flag.Getter).Get().(int),
 	}
 	lib.MakeTextToBold(stdinWord, args)
 }
